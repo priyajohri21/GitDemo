7e19b4f9e9124651c64c9bb3acbdd24b82b41023
Priyanka <priyankajohri2030@gmail.com>
2022-02-24 03:42:57 +0530

    This is for indian test

--- src/main/java/qaclickacademy/RESTAPITest.java
+++ src/main/java/qaclickacademy/RESTAPITest.java
@@ -9,6 +9,9 @@ public class RESTAPITest {
 	System.out.println("postJira1");
 	System.out.println("postJira2");
 	System.out.println("postJira3");
+	System.out.println("postJira4");
+	System.out.println("postJira5");
+
 
 	}
 	@Test
