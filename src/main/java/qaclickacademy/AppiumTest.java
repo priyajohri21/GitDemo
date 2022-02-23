e4e0c0040c7d601c27452a7b126a055863edc33d
priyanka <priyankajohri2030@gmail.com>
2022-02-24 02:28:02 +0530

    first commit

---
+++ src/main/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void nativeappandroid()
+	{
+	System.out.println("nativeappandroid");
+	}
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+	
+}
---
+++ src/main/java/qaclickacademy/RESTAPITest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	@Test
+	public void postJira()
+	{
+	System.out.println("postJira");
+	}
+	@Test
+	public void deletetwitter()
+	{
+		System.out.println("deletetwitter");
+	}
+	
+
+}
