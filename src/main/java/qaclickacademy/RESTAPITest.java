8640a987b59248c7403e4e2b9ffc3a28f8dae31d
Priya <priyankajohri2030@gmail.com>
2022-02-25 01:36:22 +0530

    feb First commit

--- src/main/java/qaclickacademy/RESTAPITest.java
+++ src/main/java/qaclickacademy/RESTAPITest.java
@@ -12,7 +12,9 @@ public class RESTAPITest {
 	System.out.println("postJira4");
 	System.out.println("postJira5");
 
-
+	System.out.println("postJira6");
+	System.out.println("postJira7");
+	System.out.println("postJira8");
 	}
 	@Test
 	public void deletetwitter()
@@ -20,5 +22,10 @@ public class RESTAPITest {
 		System.out.println("deletetwitter");
 	}
 	
-
+	@Test
+	public void deletetwitter2()
+	{
+		System.out.println("deletetwitter2");
+	}
+	
 }
